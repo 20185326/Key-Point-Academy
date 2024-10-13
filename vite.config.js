2cfe97a830a95ba5b54be9576087a440272e86f1
andre <andre.zambrano.cueva@gmail.com>
2024-10-13 00:58:13 -0500

    Push

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Key-Point-Academy/',  // Asegúrate de que 'Key-Point-Academy' coincide con el nombre de tu repositorio
 })
